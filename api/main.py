310e6e572c83c6ae5321eb34e76290d19cfcece9
Tadiwanashe Gukwa <20095319@tafe.wa.edu.au>
2024-11-20 01:33:34 +0800

    feat(step-5): implement Middleware

--- api/main.py
+++ api/main.py
@@ -1,13 +1,18 @@
 import casbin
 from fastapi import FastAPI
 import os
-
+from fastapi_authz import CasbinMiddleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+import basic_auth
 api = FastAPI()
 
 enforcer = casbin.Enforcer(os.path.join(os.path.dirname(__file__), "rbac_model.conf"),
                            os.path.join(os.path.dirname(__file__), "rbac_policy.csv"),
                            )
+baskend = basic_auth.BasicAuth()
 
+api.add_middleware(CasbinMiddleware, enforcer=enforcer)
+api.add_middleware(AuthenticationMiddleware, backend=baskend)
 @api.get('/')
 async def index():
     return 'Hello, World'
