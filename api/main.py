fbd504a4a6765f7d34ebeabebc89ec9f787d2c42
Tadiwanashe Gukwa <20095319@tafe.wa.edu.au>
2024-11-20 01:20:24 +0800

    feat(step-3): Add PBAC policies and models

--- api/main.py
+++ api/main.py
@@ -1,6 +1,12 @@
+import casbin
 from fastapi import FastAPI
+import os
 
 api = FastAPI()
+
+enforcer = casbin.Enforcer(os.path.join(os.path.dirname(__file__), "rbac_model.conf"),
+                           os.path.join(os.path.dirname(__file__), "rbac_policy.csv"),
+                           )
 
 @api.get('/')
 async def index():
