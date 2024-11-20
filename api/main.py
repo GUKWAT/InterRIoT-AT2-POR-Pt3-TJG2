41bc1615c3330413b8b28f71e56a4b783e52a74c
Tadiwanashe Gukwa <20095319@tafe.wa.edu.au>
2024-11-20 14:40:03 +0800

    feat(step-7): Authenticating Bob

--- api/main.py
+++ api/main.py
@@ -16,3 +16,11 @@ api.add_middleware(AuthenticationMiddleware, backend=baskend)
 @api.get('/')
 async def index():
     return 'Hello, World'
+
+@api.get('/ds1/res1')
+async def ds1_res1():
+    return 'alice'
+
+@api.get('/ds2/res2')
+async def ds2_res2():
+    return 'alice'
